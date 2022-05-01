3a7069f4caebd290814378e978803888b050cd8a
leeejjju <dlwlswn02027@gmail.com>
2022-05-01 15:10:17 +0900

    Create 25083.cpp

---
+++ 25083.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+//백준 25083
+//새싹 출력하기 
+/*
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+*/
+
+int main(void){
+
+    cout << "         ,r\'\"7" << endl;
+    cout << "r`-_   ,\'  ,/" << endl;
+    cout << " \\. \". L_r\'" << endl;
+    cout << "   `~\\/" << endl;
+    cout << "      |" << endl;
+    cout << "      |" << endl;
+
+
+    return 0;
+}
